8331226cdf560eb95e1a44eff539b139e92682b9
Jirawat Kusalangkurwat <60333308+lukepanter@users.noreply.github.com>
2021-07-25 09:13:02 +0000

    modify split parameter

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ async def on_message(message):
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
   if message.content.startswith('$deposit'):
-    typeMsg = message.content.split()[1]
-    Dmsg = message.content.split()[2]
+    typeMsg = message.content.split(" ", 2)[1]
+    Dmsg = message.content.split(" ", 2)[2]
     depositMsg(typeMsg,Dmsg)
     await message.channel.send('I got it!')
   if message.content.startswith('$print'):
@@ -34,6 +34,8 @@ async def on_message(message):
     keyDel = message.content.split()[1]
     del db[keyDel]
     await message.channel.send('Eliminate!')
+  #if str(message.channel) == 'ตู้เย็นผัม'and message.content.split()[0] == "$deposit" :
+    #await message.channel.purge(limit = 1)
 
 keep_alive()
 my_secret = os.environ['TOKEN']
