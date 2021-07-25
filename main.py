30277219813d70a220f6f0cb6f7c559a78026240
Jirawat Kusalangkurwat <60333308+lukepanter@users.noreply.github.com>
2021-07-25 08:29:52 +0000

    Add deposit content function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import discord
 import os
+from replit import db
 
 client = discord.Client()
+
+def depositMsg(typeMsg,Dmsg):
+  db[typeMsg] = Dmsg
+
+
 
 @client.event
 async def on_ready():
@@ -13,6 +19,21 @@ async def on_message(message):
     return
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
+  if message.content.startswith('$deposit'):
+    typeMsg = message.content.split()[1]
+    Dmsg = message.content.split()[2]
+    depositMsg(typeMsg,Dmsg)
+    await message.channel.send('I got it!')
+  if message.content.startswith('$print'):
+    keys = db.keys()
+    for x in keys:
+      newMsg = x + "   " + db[x]
+      await message.channel.send(newMsg)
+  if message.content.startswith('$delete'):  
+    keyDel = message.content.split()[1]
+    del db[keyDel]
+    await message.channel.send('Eliminate!')
+
 
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
